8836cb9098b99f386e05a6b7745252585f351632
jovighost <williamstanlet863@gmail.com>
2025-03-05 11:31:28 +0100

    file changes

---
+++ script.js
@@ -0,0 +1,108 @@
+const mobileMenu = document.getElementById("mobile_menu");
+mobileMenu.addEventListener("click",()=>{
+    const destopMenu = document.getElementByI("desktop_menu")
+    destopMenu.ClassList.toggle("active");
+    mobileMenu.classList.toggle("active");
+    
+})
+// An Array for card objects
+
+
+
+
+const projectCards = [
+    {Image :"",
+        tittle: "Multi-post Stories Gain+Glory",
+        technoloies: [
+            "Ruby On Rails",
+            "CSS",
+            "JavaScript",
+        ],
+        link:"#",
+
+
+
+    },
+    {image :"",
+        tittle: "Multi-post Stories Gain+Glory",
+        technoloies: [
+            "Ruby On Rails",
+            "CSS",
+            "JavaScript",
+        ],
+        link:"#"
+
+    },
+    {image :"",
+        tittle: "Multi-post Stories Gain+Glory",
+        technoloies: [
+            "Ruby On Rails",
+            "CSS",
+            "JavaScript",
+        ],
+        link:"#"
+
+    },
+    {image :"",
+        tittle: "Multi-post Stories Gain+Glory",
+        technoloies: [
+            "Ruby On Rails",
+            "CSS",
+            "JavaScript",
+        ],
+        link:"#"
+
+    },
+    {image :"",
+        tittle: "Multi-post Stories Gain+Glory",
+        technoloies: [
+            "Ruby On Rails",
+            "CSS",
+            "JavaScript",
+        ],
+        link:"#"
+
+    },
+    {image :"",
+        tittle: "Multi-post Stories Gain+Glory",
+        technoloies: [
+            "Ruby On Rails",
+            "CSS",
+            "JavaScript",
+        ],
+        link:"#"
+
+    }
+]
+// function to create the above card
+function createProjectCards(project){
+    const card = document.createElement('div');
+    card.classList.add("project_card");
+    const picture = document.createElement("img");
+    picture.src = project.image;
+    picture.alt = project.tittle;
+    card.appendChild(picture);
+    const titre  = document.createElement("h3")
+    titre.innerHTML = project.tittle;
+    card.appendChild(titre);
+    const list = document.createElement ("ul");
+    project.technoloies.forEach(element =>{
+        const listitem = document.createElement("li");
+        listitem.innerHTML = element;
+        list.appendChild(listitem)
+    })
+    card.appendChild(list);
+    const cardBtn = document.createElement("button");
+    card.appendChild(cardBtn);
+    cardBtn.innerHTML = 'See Project';
+    return card;
+}
+// function to display proiject card
+function displayProjectCard(){
+    const createProjectCardsContainer = document.querySelector('.projects_cards');
+    projectCards.forEach((project)=>{
+        const projectCard = createProjectCards(project);
+        createProjectCardsContainer.appendChild(projectCard)
+    })
+}
+document.addEventListener('DOMContentLoaded', displayProjectCard);
